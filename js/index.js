40143b01c02ae419ef75dfb5051c0d6e7227131a
Darlan <Darlan.silvadacruz@gmail.com>
2021-02-26 10:47:24 -0300

    Menu lateral para mobile configurado

--- js/index.js
+++ js/index.js
@@ -2,13 +2,23 @@ import { newTask } from './newTask.js';
 import { load } from './loadTasks.js';
 const inputTask = document.getElementsByClassName("new-task-area__input-task")[0];
 const inputDate = document.getElementsByClassName("new-task-area__input-date")[0];
-const importantButton = document.getElementsByClassName("task__important-button");
-
+const buttonMenuMobile = document.getElementsByClassName("topbar-mobile__menu-hamburger")[0];
+const buttonSideMenuMobile = document.getElementsByClassName("menu__menu-hamburger")[0];
 load();
 
 inputTask.addEventListener("keyup", (event) => {
   if (event.key == "Enter" && inputTask.value && inputDate.value) {
-    const tasks = newTask();
+    newTask();
     load();
   }
+});
+
+buttonMenuMobile.addEventListener("click", () => {
+  const sideMenu = document.getElementsByClassName("menu")[0];
+  sideMenu.classList.add("menu-mobile--open");
+});
+
+buttonSideMenuMobile.addEventListener("click", () => {
+  const sideMenu = document.getElementsByClassName("menu")[0];
+  sideMenu.classList.remove("menu-mobile--open");
 });
