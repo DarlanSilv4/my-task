47349669d6b309808789b6a155afcdbbe3e41c85
Darlan <Darlan.silvadacruz@gmail.com>
2021-03-04 10:32:02 -0300

    Menu mobile fecha ao clicar fora do container

--- js/mobile.js
+++ js/mobile.js
@@ -1,6 +1,14 @@
 export const setupMobileMenu = () => {
+    const menu = document.getElementsByClassName("menu")[0];
     const buttonMenuMobile = document.getElementsByClassName("topbar-mobile__menu-hamburger")[0];
     const buttonSideMenuMobile = document.getElementsByClassName("menu__menu-hamburger")[0];
+
+    menu.addEventListener("click", (event) => {
+        if (event.target.classList[1] == "menu-mobile--open") {
+            const sideMenu = document.getElementsByClassName("menu")[0];
+            sideMenu.classList.remove("menu-mobile--open");
+        }
+    });
 
     buttonMenuMobile.addEventListener("click", () => {
         const sideMenu = document.getElementsByClassName("menu")[0];
