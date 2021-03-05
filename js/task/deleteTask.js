465bb533aa0e41b699c20aa116b0204a55bafd56
Darlan <Darlan.silvadacruz@gmail.com>
2021-03-05 10:34:06 -0300

    Mostra modal de alerta antes de deletar task

--- js/task/deleteTask.js
+++ js/task/deleteTask.js
@@ -8,7 +8,7 @@ export const deleteButton = (id) => {
                         <path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/>
                         </svg>`
 
-    button.addEventListener('click', () => deleteTask(id));
+    button.addEventListener('click', () => showModal(id));
     return button;
 }
 
@@ -23,5 +23,39 @@ const deleteTask = (id) => {
     });
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
+    hideModal();
     load();
+}
+
+const showModal = (id) => {
+    const modal = document.getElementsByClassName('modal-delete')[0];
+    modal.classList.remove('--hide-modal');
+
+    const confirmButton = document.getElementsByClassName('confirm')[0];
+    confirmButton.addEventListener('click', () => {
+        deleteTask(id)
+    });
+
+    const cancelButton = document.getElementsByClassName('cancel')[0];
+    cancelButton.addEventListener('click', () => {
+        hideModal();
+    })
+
+    modal.addEventListener('click', (event) => {
+        if (event.target.classList[0] == 'modal-delete') {
+            hideModal();
+        }
+    });
+}
+
+const hideModal = () => {
+    const modal = document.getElementsByClassName('modal-delete')[0];
+    removeConfirmButtonEventListener();
+    modal.classList.add('--hide-modal');
+}
+
+const removeConfirmButtonEventListener = () => {
+    const confirmButton = document.getElementsByClassName('confirm')[0];
+    const newConfirmButton = confirmButton.cloneNode('true');
+    confirmButton.parentNode.replaceChild(newConfirmButton, confirmButton);
 }
